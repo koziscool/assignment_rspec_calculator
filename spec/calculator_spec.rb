3b7c95b860e987661971eccb73bb7b351bf800da
koziscool <kozjob@yahoo.com>
2016-01-07 12:13:32 -0600

    some bugfixes, refactoring

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,24 +6,43 @@ describe "Calculator without stringify" do
     
     let( :calculator ) { Calculator.new }
 
-    it "adds correctly" do
+    it "adds integers correctly" do
         expect( calculator.add(2, 2)).to eq(4) 
+    end
+
+    it "adds floats correctly" do
         expect( calculator.add(2.0, 3.3)).to be_within(0.1).of(5.3) 
+    end
+    it "adds negative numbers correctly" do
         expect( calculator.add(-2, 3.3)).to be_within(0.1).of(1.3)
+    end
+    it "adds negative numbers correctly" do
         expect( calculator.add(-2, -5)).to eq(-7)
     end
 
     it "subtracts correctly" do
         expect( calculator.subtract(2, 1)).to eq(1) 
+    end
+    it "subtracts floats correctly" do
         expect( calculator.subtract(2.0, 3.3)).to be_within(0.1).of(-1.3) 
-        expect( calculator.subtract(-2, 3.3)).to be_within(0.1).of(-5.3)
+    end
+    it "subtracts negative numbers correctly" do
+        expect( calculator.subtract(-2, -3.3)).to be_within(0.1).of(1.3)
+    end
+    it "subtracts negative numbers correctly" do
         expect( calculator.subtract(-2, -5)).to eq(3)
     end
     
     it "multiplies correctly" do
         expect( calculator.multiply(2, 1)).to eq(2) 
+    end
+    it "multiplies floats correctly" do
         expect( calculator.multiply(2.0, 3.3)).to be_within(0.1).of(6.6) 
+    end
+    it "multiplies negative numbers correctly" do
         expect( calculator.multiply(-2, 3.3)).to be_within(0.1).of(-6.6)
+    end
+    it "multiplies negative numbers correctly" do
         expect( calculator.multiply(-2, -5)).to eq(10)
     end
 
@@ -31,19 +50,19 @@ describe "Calculator without stringify" do
         expect( calculator.divide(2, 1)).to eq(2) 
     end    
     
-    it "divides correctly" do
+    it "divides floats correctly" do
         expect( calculator.divide(3.3, 2.0)).to be_within(0.1).of(1.65) 
     end
 
-    it "calculates power correctly" do
-        expect( calculator.divide(-2, 3.3)).to be_within(0.1).of(-0.6)
+    it "divides floats to floats correctly" do
+        expect( calculator.divide(-2, 3.3)).to be_a( Float )
     end
     
-    it "calculates power correctly" do    
+    it "divides negative numbers correctly" do
         expect( calculator.divide(-2, -5)).to eq(0.4)
     end
 
-    it "divides by 0 correctly" do    
+    it " handles divide by 0 correctly" do    
         expect {
             calculator.divide(10, 0)
         }.to raise_error(ArgumentError)
@@ -59,19 +78,15 @@ describe "Calculator without stringify" do
     it "calculates power correctly" do        
         expect( calculator.pow(3.3, 2.0)).to be_within(0.1).of(10.9) 
     end    
-    it "calculates power correctly" do        
-        expect( calculator.pow(-2, 3)).to be_within(0.1).of(-8)
-    end    
-    it "calculates power correctly" do        
-        expect( calculator.pow(-5, 2)).to eq(25)
-    end
-
+ 
     it "calculates square root of perfect square correctly" do
         expect( calculator.sqrt(25)).to eq(5) 
+        expect( calculator.sqrt(25)).to be_a(Fixnum) 
     end    
    
     it "calculates square root of imperfect square correctly" do
         expect( calculator.sqrt(10)).to be_within(0.1).of(3.1) 
+        expect( calculator.sqrt(10)).to be_a(Float) 
     end
 
     it "raises error for square root of negative number" do    
