5039434907884c4b971a1afdf4f5c9037e26c2f4
Deepa Kackar <dkackar99@yahoo.com>
2016-01-07 09:58:51 -0800

    Added power, stringify

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,7 +2,7 @@
 
 require "calculator"
 
-describe "add" do 
+describe "Calculator without stringify" do 
     
     let( :calculator ) { Calculator.new }
 
@@ -27,15 +27,75 @@ describe "add" do
         expect( calculator.multiply(-2, -5)).to eq(10)
     end
 
-    it "divides correctly" do
+    it "divides 2 positive integers correctly" do
         expect( calculator.divide(2, 1)).to eq(2) 
+    end    
+    
+    it "divides correctly" do
         expect( calculator.divide(3.3, 2.0)).to be_within(0.1).of(1.65) 
+    end
+
+    it "calculates power correctly" do
         expect( calculator.divide(-2, 3.3)).to be_within(0.1).of(-0.6)
+    end
+    
+    it "calculates power correctly" do    
         expect( calculator.divide(-2, -5)).to eq(0.4)
     end
 
- 
+    it "divides by 0 correctly" do    
+        expect {
+            calculator.divide(10, 0)
+        }.to raise_error(ArgumentError)
+    end
 
+    it "calculates to positive power correctly" do
+        expect( calculator.pow(2, 1)).to eq(2) 
+    end
+    it "calculates power correctly" do
+        expect( calculator.pow(2, 0)).to eq(1)
+    end    
+   
+    it "calculates power correctly" do        
+        expect( calculator.pow(3.3, 2.0)).to be_within(0.1).of(10.9) 
+    end    
+    it "calculates power correctly" do        
+        expect( calculator.pow(-2, 3)).to be_within(0.1).of(-8)
+    end    
+    it "calculates power correctly" do        
+        expect( calculator.pow(-5, 2)).to eq(25)
+    end
+
+    it "calculates square root of perfect square correctly" do
+        expect( calculator.sqrt(25)).to eq(5) 
+    end    
+   
+    it "calculates square root of imperfect square correctly" do
+        expect( calculator.sqrt(10)).to be_within(0.1).of(3.1) 
+    end
+
+    it "raises error for square root of negative number" do    
+        expect {
+            calculator.sqrt(-25)
+        }.to raise_error(ArgumentError)
+    end
+
+    #Memory
+
+    # it "saves to memory value assigned" do 
+    #     #x = 10  
+    #     #calculator.memory = x
+    #     expect( calculator.mem=10).to be_eq(10)
+    # end
 end
 
+
+describe "Calculator with stringify" do 
+    let( :calculator ) { Calculator.new(true) }
+
+    it "strigifies" do
+        expect( calculator.add(2, 2)).to eq("4") 
+    end
+
+end
 
